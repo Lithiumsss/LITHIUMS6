b6b1f27784233a1c006ea90529be7db5a456df9d
Lithiumsss <empresarcap2025@gmail.com>
2026-09-12 09:44:53 -0500

    Refactor LITHIUMS6.py for improved functionality

--- LITHIUMS6.py
+++ LITHIUMS6.py
@@ -143,7 +143,7 @@ def run():
     dr = iniciar()
     pid = dr.service.process.pid
     total_disparos_global = 0
-    MAX_DISPAROS_CICLO = 5000
+    MAX_DISPAROS_CICLO = 1200
     
     try:
         handles = abrir_y_verificar_tags_todos(dr)
